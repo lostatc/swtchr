66580362564b30d3ceafbebb2fe3feb352fd67e7
Wren Powell <wrenp@duck.com>
2024-03-04 07:18:39 -0500

    Add Hello World example

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,21 @@
-fn main() {}
+use gtk::prelude::*;
+use gtk::{glib, Application, ApplicationWindow};
+
+const APP_ID: &'static str = "io.github.lostatc.swtchr";
+
+fn main() -> glib::ExitCode {
+    let app = Application::builder().application_id(APP_ID).build();
+
+    app.connect_activate(build_window);
+
+    app.run()
+}
+
+fn build_window(app: &Application) {
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("Window switcher")
+        .build();
+
+    window.present();
+}
