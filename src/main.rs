b2e6ea281e4875c3359764f26ab4f62f8715ec35
Wren Powell <wrenp@duck.com>
2024-03-04 07:39:59 -0500

    Set up window as a Layer Shell overlay

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
+use gtk::gio::ActionEntry;
 use gtk::prelude::*;
-use gtk::{glib, Application, ApplicationWindow};
+use gtk::{glib, Application, ApplicationWindow, Label};
+use gtk4_layer_shell::{KeyboardMode, Layer, LayerShell};
 
-const APP_ID: &'static str = "io.github.lostatc.swtchr";
+const APP_ID: &str = "io.github.lostatc.swtchr";
 
 fn main() -> glib::ExitCode {
     let app = Application::builder().application_id(APP_ID).build();
 
     app.connect_activate(build_window);
+
+    // Close window with Esc key.
+    app.set_accels_for_action("win.close", &["Escape"]);
 
     app.run()
 }
@@ -14,8 +19,25 @@ fn main() -> glib::ExitCode {
 fn build_window(app: &Application) {
     let window = ApplicationWindow::builder()
         .application(app)
-        .title("Window switcher")
+        .title("swtchr window switcher")
         .build();
+
+    // Set this window up as an overlay that captures all keyboard events via the Wayland Layer
+    // Shell protocol.
+    window.init_layer_shell();
+    window.set_layer(Layer::Overlay);
+    window.set_keyboard_mode(KeyboardMode::Exclusive);
+
+    // Close window with Esc key.
+    let action_close = ActionEntry::builder("close")
+        .activate(|window: &ApplicationWindow, _, _| {
+            window.close();
+        })
+        .build();
+    window.add_action_entries([action_close]);
+
+    let label = Label::new(Some("My Window Switcher"));
+    window.set_child(Some(&label));
 
     window.present();
 }
