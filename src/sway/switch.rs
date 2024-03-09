3acfdf6822b254664b7dc235ad0296cfd31fe00a
Wren Powell <wrenp@duck.com>
2024-03-09 16:03:23 -0500

    Reuse an existing Sway socket connection

--- src/sway/switch.rs
+++ src/sway/switch.rs
@@ -1,17 +1,25 @@
+use std::sync::{Mutex, OnceLock};
+
 use eyre::WrapErr;
 use swayipc::Connection;
 
 use super::subscribe::SwayWindowId;
 
-pub fn switch_window(id: SwayWindowId) -> eyre::Result<()> {
-    let mut connection = Connection::new().wrap_err("failed acquiring a Sway IPC connection")?;
+fn connection() -> &'static Mutex<Connection> {
+    static CONNECTION: OnceLock<Mutex<Connection>> = OnceLock::new();
 
-    connection
+    CONNECTION.get_or_init(|| {
+        Mutex::new(Connection::new().expect("failed acquiring a Sway IPC connection"))
+    })
+}
+
+pub fn switch_window(id: SwayWindowId) -> eyre::Result<()> {
+    connection()
+        .lock()
+        .expect("lock is poisoned")
         .run_command(format!("[con_id=\"{}\"] focus", id.0))
-        .wrap_err("failed running Sway window switch command over IPC API")?
+        .wrap_err("failed running Sway window switch command")?
         .into_iter()
         .collect::<Result<_, _>>()
-        .wrap_err("failed running Sway window switch command over IPC API")?;
-
-    Ok(())
+        .wrap_err("failed running Sway window switch command")
 }
