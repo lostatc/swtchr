86fd3527a7cf11c8349c6ba1d48453583f001df4
Wren Powell <wrenp@duck.com>
2024-03-11 08:25:03 -0400

    Implement basic client for sending IPC commands

---
+++ src/client/main.rs
@@ -0,0 +1,15 @@
+use std::env;
+use std::os::unix::net::UnixDatagram;
+use std::str::FromStr;
+
+use swtchr::{sock_path, Command};
+
+fn main() -> eyre::Result<()> {
+    let socket = UnixDatagram::unbound()?;
+    socket.connect(sock_path()?)?;
+
+    let msg = Command::from_str(&env::args().nth(1).unwrap())?;
+    socket.send(msg.msg().as_bytes())?;
+
+    Ok(())
+}
